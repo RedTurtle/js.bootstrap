d42d87bf269396fb505ca51883cda731ced9049c
Andrew Mleczko <andrew@mleczko.net>
2012-01-17 15:06:41 +0100

    new 2.0a version

--- js/bootstrap/__init__.py
+++ js/bootstrap/__init__.py
@@ -5,23 +5,34 @@ library = Library('bootstrap', 'resources')
 
 bootstrap_css = Resource(library, 'bootstrap.css',
                          minified='bootstrap.min.css')
-bootstrap_alerts = Resource(library, 'bootstrap-alerts.js',
+
+bootstrap_alerts = Resource(library, 'bootstrap-alert.js',
                             depends=[jquery,])
+bootstrap_buttons = Resource(library, 'bootstrap-button.js',
+                          depends=[jquery,])
 bootstrap_dropdown = Resource(library, 'bootstrap-dropdown.js',
                               depends=[jquery,])
 bootstrap_modal = Resource(library, 'bootstrap-modal.js',
                            depends=[jquery,])
-bootstrap_twipsy = Resource(library, 'bootstrap-twipsy.js',
-                            depends=[jquery,])
+bootstrap_tabs = Resource(library, 'bootstrap-tab.js',
+                          depends=[jquery,])
+bootstrap_tooltip = Resource(library, 'bootstrap-tooltip.js',
+                             depends=[jquery,])
 bootstrap_popover = Resource(library, 'bootstrap-popover.js',
-                             depends=[jquery, bootstrap_twipsy])
+                             depends=[jquery, bootstrap_tooltip])
 bootstrap_scrollspy = Resource(library, 'bootstrap-scrollspy.js',
                                depends=[jquery,])
-bootstrap_tabs = Resource(library, 'bootstrap-tabs.js',
-                          depends=[jquery,])
-bootstrap_buttons = Resource(library, 'bootstrap-buttons.js',
-                          depends=[jquery,])
+bootstrap_carousel = Resource(library, 'bootstrap-carousel.js',
+                               depends=[jquery,])
+bootstrap_collapse = Resource(library, 'bootstrap-collapse.js',
+                               depends=[jquery,])
+bootstrap_transition = Resource(library, 'bootstrap-transition.js',
+                               depends=[jquery,])
+bootstrap_typeahead = Resource(library, 'bootstrap-typeahead.js',
+                               depends=[jquery,])
 
 bootstrap = Group([bootstrap_css, bootstrap_alerts, bootstrap_dropdown,
                    bootstrap_modal, bootstrap_popover, bootstrap_scrollspy,
-                   bootstrap_tabs, bootstrap_twipsy, bootstrap_buttons])
+                   bootstrap_tabs, bootstrap_tooltip, bootstrap_buttons,
+                   bootstrap_carousel, bootstrap_collapse, bootstrap_transition, bootstrap_typeahead
+                   ])
