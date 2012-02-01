5f46f3886c16cf8ae0909d418094224ba084b6b2
Andrew Mleczko <andrew@mleczko.net>
2012-02-01 11:48:01 +0100

    started using bootstrap 2.0

--- js/bootstrap/__init__.py
+++ js/bootstrap/__init__.py
@@ -5,23 +5,11 @@ library = Library('bootstrap', 'resources')
 
 bootstrap_css = Resource(library, 'bootstrap.css',
                          minified='bootstrap.min.css')
-bootstrap_alerts = Resource(library, 'bootstrap-alerts.js',
-                            depends=[jquery,])
-bootstrap_dropdown = Resource(library, 'bootstrap-dropdown.js',
-                              depends=[jquery,])
-bootstrap_modal = Resource(library, 'bootstrap-modal.js',
-                           depends=[jquery,])
-bootstrap_twipsy = Resource(library, 'bootstrap-twipsy.js',
-                            depends=[jquery,])
-bootstrap_popover = Resource(library, 'bootstrap-popover.js',
-                             depends=[jquery, bootstrap_twipsy])
-bootstrap_scrollspy = Resource(library, 'bootstrap-scrollspy.js',
-                               depends=[jquery,])
-bootstrap_tabs = Resource(library, 'bootstrap-tabs.js',
-                          depends=[jquery,])
-bootstrap_buttons = Resource(library, 'bootstrap-buttons.js',
-                          depends=[jquery,])
+bootstrap_responsive_css = Resource(library, 'bootstrap.responsive',
+                                    minified='bootstrap.min.responsive',
+                                    depends=[bootstrap_css,],)
+bootstrap_js = Resource(library, 'bootstrap.js',
+                        minified='bootstrap.min.js',
+                        depends=[jquery,])
 
-bootstrap = Group([bootstrap_css, bootstrap_alerts, bootstrap_dropdown,
-                   bootstrap_modal, bootstrap_popover, bootstrap_scrollspy,
-                   bootstrap_tabs, bootstrap_twipsy, bootstrap_buttons])
+bootstrap = Group([bootstrap_css, bootstrap_js])
