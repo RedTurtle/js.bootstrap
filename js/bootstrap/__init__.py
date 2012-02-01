87d6e25ddddc984bbf09c17c158b3a86aa661bfc
Andrew Mleczko <andrew@mleczko.net>
2012-02-01 15:30:29 +0100

    add images

--- js/bootstrap/__init__.py
+++ js/bootstrap/__init__.py
@@ -3,13 +3,13 @@ from js.jquery import jquery
 
 library = Library('bootstrap', 'resources')
 
-bootstrap_css = Resource(library, 'bootstrap.css',
-                         minified='bootstrap.min.css')
-bootstrap_responsive_css = Resource(library, 'bootstrap.responsive.css',
-                                    minified='bootstrap.min.responsive.css',
+bootstrap_css = Resource(library, 'css/bootstrap.css',
+                         minified='css/bootstrap.min.css')
+bootstrap_responsive_css = Resource(library, 'css/bootstrap.responsive.css',
+                                    minified='css/bootstrap.min.responsive.css',
                                     depends=[bootstrap_css,],)
-bootstrap_js = Resource(library, 'bootstrap.js',
-                        minified='bootstrap.min.js',
+bootstrap_js = Resource(library, 'js/bootstrap.js',
+                        minified='js/bootstrap.min.js',
                         depends=[jquery,])
 
 bootstrap = Group([bootstrap_css, bootstrap_js])
