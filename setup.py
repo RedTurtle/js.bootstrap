7074b5f7b5e5ed1318e8b4eba59793c7cff0acf8
Andrew Mleczko <andrew@mleczko.net>
2012-03-02 11:59:15 +0100

    update readme filename

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 long_description = (
-    read('README.txt')
+    read('README.rst')
     + '\n' +
     read('js', 'bootstrap', 'test_bootstrap.txt')
     + '\n' +
