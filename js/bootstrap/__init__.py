8629f20d4f5d75907f4110df3db6b44b8bf38690
Jeff Dairiki <dairiki@dairiki.org>
2013-09-30 14:40:53 -0700

    Package bootstrap-theme.css too.
    Bootstrap-theme.css is distributed in the main bootstrap zip-ball.
    It provides drop-shadow and gradients on buttons, menus, panels &c
    for those who want them.

--- js/bootstrap/__init__.py
+++ js/bootstrap/__init__.py
@@ -5,6 +5,11 @@ library = Library('bootstrap', 'resources')
 
 bootstrap_css = Resource(library, 'css/bootstrap.css',
                          minified='css/bootstrap.min.css')
+
+bootstrap_theme = Resource(library, 'css/bootstrap-theme.css',
+                           minified='css/bootstrap-theme.min.css',
+                           depends=[bootstrap_css])
+
 bootstrap_js = Resource(library, 'js/bootstrap.js',
                         minified='js/bootstrap.min.js',
                         depends=[jquery, ])
